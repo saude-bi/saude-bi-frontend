710663f111495c868e7ae985863525e1ff22383e
edua2127 <magalhaeseduardo2001@gmail.com>
2023-05-22 19:18:43 -0300

    adiciona um comentario descrevendo a função

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -7,11 +7,23 @@ interface FetchState<T> {
   error: AxiosError | null;
 }
 
-/* 
-    useFetch<T = unknown>(url: string, options?: AxiosRequestConfig): FetchState<T> 
-    o que faz <T = unknown> ? define a classe que vai ser usada dentro da função
-    o que faz FetchState<T> ? define o retorno da função
-*/
+/**
+ * useFetch é um hook personalizado para realizar uma chamada HTTP e retornar os dados, 
+ * o estado de carregamento e qualquer erro que possa ocorrer.
+ *
+ * @param url A URL para a qual a chamada HTTP deve ser feita.
+ * @param options Opções opcionais para a chamada HTTP (ex: método, cabeçalhos, etc.).
+ * @returns Um objeto com três propriedades:
+ *  - data: os dados retornados pela chamada HTTP. O tipo de dados é definido por T.
+ *  - loading: um booleano que indica se a chamada HTTP ainda está em progresso.
+ *  - error: um erro que ocorreu durante a chamada HTTP, se houver.
+ *
+ * O tipo genérico T é usado para definir o formato esperado dos dados de resposta. 
+ * Se nenhum tipo é especificado, ele é considerado 'unknown' por padrão.
+ *
+ * Exemplo de uso:
+ * const { data, loading, error } = useFetch<UserData>('https://api.example.com/user');
+ */
 function useFetch<T = unknown>(url: string, options?: AxiosRequestConfig): FetchState<T> {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
