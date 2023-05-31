05701ef7c7d02ef89a2aab50161671b47c4c250a
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-05-31 01:43:45 -0300

    feat: add user profile

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Stack } from '@mantine/core';
+import { Stack, Title, Text, Group, ActionIcon } from '@mantine/core';
 import { User } from '@/types/user';
+import { IconUser } from '@tabler/icons-react';
 
 type Props = {
   user: User;
 };
 
 export const UserProfile: React.FC<Props> = ({ user }) => {
-  return <Stack bg="red" h="50px" w="50px"></Stack>;
+  return (
+    <Group spacing="lg">
+      <ActionIcon color="orange.3" variant="filled" size="xl" radius="md">
+        <IconUser size="32px" />
+      </ActionIcon>
+
+      <Stack spacing={0}>
+        <Title color="black" weight="bold" size="md">
+          {user.username}
+        </Title>
+        <Text color="dark.3" size="xs">
+          {user.username}
+        </Text>
+      </Stack>
+    </Group>
+  );
 };
