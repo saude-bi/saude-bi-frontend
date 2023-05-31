823b748b3aea4f3ec9938309ed2538fb77941679
root <root@DESKTOP-9AMB61P>
2023-05-31 19:37:40 -0300

    feat: Add mantine react table

---
+++ src/pages/occupation-categories.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useFindAllDashboardsQuery } from '@/store/dashboards';
+import { CommonLayout } from '@/components/Common/Layout/CommonLayout';
+
+export default function Home() {
+  const { data: dashboards } = useFindAllDashboardsQuery();
+
+  useEffect(() => {
+    console.log(dashboards);
+  }, [dashboards]);
+
+  return (
+    <CommonLayout title="Dashboards">
+      <p>Bem Vindo!</p>
+    </CommonLayout>
+  );
+}
