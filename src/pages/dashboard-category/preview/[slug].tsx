fd286744808b1405da54665aa41916befd54d79d
helcio <helcio@carmo.in>
2023-06-08 00:30:16 -0300

    refactor: Remove put code from update

--- src/pages/dashboard-category/preview/[slug].tsx
+++ src/pages/dashboard-category/preview/[slug].tsx
@@ -1,23 +1,16 @@
 import { FormLayout } from '@/components/Common/Layout/FormLayout';
-import {
-  OccupationCategory,
-  useCreateOccupationCategoryMutation,
-  useRemoveOccupationCategoryMutation,
-  useFindAllOccupationCategoriesQuery,
-  useUpdateOccupationCategoryMutation,
-} from '@/store/occupation-categories';
-import { Button, Grid, LoadingOverlay, Stack, Text, TextInput } from '@mantine/core';
-import React, { useEffect, useMemo, useRef } from 'react';
-import { DashboardCategory, DashboardCategoryDto } from '@/types/dashboard-category'
+import React, { useEffect } from 'react';
+import { DashboardCategoryDto } from '@/types/dashboard-category'
 import { useForm } from '@mantine/form';
-import { DashboardCategoryFormProvider, DashboardCategoryInputs } from '@/components/Forms/dashboard-category';
-import { notifications } from '@mantine/notifications';
-import { useUpdateDashboardCategoryMutation, useFindDashboardCategoryQuery, useRemoveDashboardCategoryMutation } from '@/store/dashboard-categories';
+import { DashboardCategoryInputs } from '@/components/Forms/dashboard-category';
+import { 
+  useFindDashboardCategoryQuery, 
+  useRemoveDashboardCategoryMutation
+} from '@/store/dashboard-categories';
 import { useRouter } from 'next/router';
 import { skipToken } from '@reduxjs/toolkit/dist/query';
 
 export default function OccupationCategoriesPage() {
-  const [submit, { isSuccess: isSaveSuccess, isError: isSaveError }] = useUpdateDashboardCategoryMutation();
   const router = useRouter();
   const { slug } = router.query;
   const id = parseInt(slug as string, 10);
@@ -38,25 +31,12 @@ export default function OccupationCategoriesPage() {
     validateInputOnChange: true
   });
 
-  const trigerSubmit = () => {
-    if (!form.validate().hasErrors) {
-      submit({ id, body: form.values})
-    }
-  };
-
   useEffect(() => {
     if (isSuccess) {
       console.log(data)
       form.setValues(data);
     }
-
-    if (isSaveSuccess) {
-      router.push('/dashboard-category');
-    }
-    if (isSaveError) {
-      notifications.show({ message: 'Erro ao cadastrar uma Categoria', color: 'red' })
-    }
-  }, [isSaveSuccess, isSuccess]);
+  }, [isSuccess]);
 
   return (
     <FormLayout 
