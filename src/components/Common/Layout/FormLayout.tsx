1386f7c749bc5cde34c5d39dd10567411a52ceb2
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-06-08 00:45:31 -0300

    feat: improve typing in FormLayout

--- src/components/Common/Layout/FormLayout.tsx
+++ src/components/Common/Layout/FormLayout.tsx
@@ -1,104 +1,86 @@
-import { ButtonBack, ButtonDelete, ButtonEdit, ButtonSave } from '@/components/Common/Buttons/Buttons';
+import {
+  ButtonBack,
+  ButtonDelete,
+  ButtonEdit,
+  ButtonSave,
+} from '@/components/Common/Buttons/Buttons';
 import { Grid, Stack, Text } from '@mantine/core';
 import { CommonLayout } from '@/components/Common/Layout/CommonLayout';
 import { ContentCard } from '../ContentCard/ContentCard';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { UseMutation } from '@reduxjs/toolkit/dist/query/react/buildHooks';
-import { 
-  BaseQueryFn,
-  FetchArgs,
-  FetchBaseQueryError,
-  FetchBaseQueryMeta,
-  MutationDefinition,
-} from '@reduxjs/toolkit/dist/query';
 import { UseForm } from '@mantine/form/lib/types';
-import { DashboardCategory } from '@/types/dashboard-category';
+import {
+  GenericCreateMutation,
+  GenericRemoveMutation,
+  GenericUpdateMutation,
+} from '@/store/common';
+
+type GenericForm<T> = ReturnType<UseForm<T>>;
 
 type Props<T> = {
-  children: React.ReactNode,
-  title: string,
-  form: ReturnType<UseForm<T>>,
-  FormInputs: React.FC<{ disabled: boolean, form: ReturnType<UseForm<T>> }>
-} & (PropsPreviewActions & { type: 'preview' } | PropsSaveAction<T> & { type: 'create'  } | PropsUpdateAction<T> & { type: 'update' })
+  title: string;
+  form: GenericForm<T>;
+  FormInputs: React.FC<{ disabled: boolean; form: GenericForm<T> }>;
+} & (
+  | (PropsPreviewActions & { type: 'preview' })
+  | (PropsCreateAction<T> & { type: 'create' })
+  | (PropsUpdateAction<T> & { type: 'update' })
+);
 
-export const FormLayout = <T,>({ 
-  children,
-  title,
-  form,
-  FormInputs,
-  ...props
-}: Props<T>) => {
+export const FormLayout = <T,>({ title, form, FormInputs, ...props }: Props<T>) => {
   const router = useRouter();
 
   const handleBack = () => {
     router.back();
-  }
-  
+  };
+
   return (
     <CommonLayout title={title}>
-        <Grid>
-            <Grid.Col span={3}>
-                <ContentCard>
-                    <Stack>
-                        <Text color="dark.3">
-                            Ações
-                        </Text>
-                        {props.type === 'create' &&
-                          <CreateAction 
-                            form={form} 
-                            useSaveMutation={props.useSaveMutation}
-                          />
-                        }
-                        {props.type === 'update' &&
-                          <UpdateAction
-                            id={props.id} 
-                            form={form} 
-                            useSaveMutation={props.useSaveMutation}
-                          />
-                        }
-                        {props.type === 'preview' &&
-                          <PreviewAction {...props} />
-                        }
-                        <ButtonBack onClick={handleBack} />
-                    </Stack>
-                </ContentCard>
-            </Grid.Col>
-            <Grid.Col span={9}>
-                <ContentCard>
-                  <form onSubmit={form.onSubmit(() => {})}>
-                    <FormInputs disabled={props.type === 'preview'} form={form} ></FormInputs>
-                  </form>
-                </ContentCard>
-            </Grid.Col>
-        </Grid>
+      <Grid>
+        <Grid.Col span={3}>
+          <ContentCard>
+            <Stack>
+              <Text color="dark.3">Ações</Text>
+              {props.type === 'create' && (
+                <CreateAction form={form} useCreateMutation={props.useCreateMutation} />
+              )}
+              {props.type === 'update' && (
+                <UpdateAction
+                  id={props.id}
+                  form={form}
+                  useUpdateMutation={props.useUpdateMutation}
+                />
+              )}
+              {props.type === 'preview' && <PreviewAction {...props} />}
+              <ButtonBack onClick={handleBack} />
+            </Stack>
+          </ContentCard>
+        </Grid.Col>
+        <Grid.Col span={9}>
+          <ContentCard>
+            <form onSubmit={form.onSubmit(() => {})}>
+              <FormInputs disabled={props.type === 'preview'} form={form}></FormInputs>
+            </form>
+          </ContentCard>
+        </Grid.Col>
+      </Grid>
     </CommonLayout>
   );
 };
 
 type PropsPreviewActions = {
-  updateUrl: string,
-  id: number,
-  useRemoveMutation: UseMutation<
-    MutationDefinition<
-      number,
-      BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>,
-      string,
-      null
-    >
-  >
+  updateUrl: string;
+  id: number;
+  useRemoveMutation: GenericRemoveMutation;
 };
 
-const PreviewAction = ({
-  updateUrl,
-  id,
-  useRemoveMutation
-}: PropsPreviewActions) => {
+const PreviewAction = ({ updateUrl, id, useRemoveMutation }: PropsPreviewActions) => {
   const router = useRouter();
   const [remove, { isSuccess }] = useRemoveMutation();
 
   const onDelete = () => {
-    remove(id)
+    remove(id);
   };
 
   useEffect(() => {
@@ -113,78 +95,53 @@ const PreviewAction = ({
       <ButtonDelete onClick={onDelete} />
     </>
   );
+};
+
+interface PropsCreateAction<T> {
+  form: GenericForm<T>;
+  useCreateMutation: GenericCreateMutation<any, T>;
 }
 
-interface PropsSaveAction<T> {
-  form: ReturnType<UseForm<T>>,
-  useSaveMutation: UseMutation<
-    MutationDefinition<
-      T,
-      BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>, 
-      string,
-      unknown
-    >
-  >
-}
-
-const CreateAction = <T,>({
-  form,
-  useSaveMutation
-}: PropsSaveAction<T>) => {
+const CreateAction = <T,>({ form, useCreateMutation }: PropsCreateAction<T>) => {
   const router = useRouter();
-  const [save, { isSuccess }] = useSaveMutation();
+  const [save, { isSuccess }] = useCreateMutation();
 
   const onSave = () => {
     if (form.validate().hasErrors) {
-      save(form.values)
-    } 
-  }
+      save(form.values);
+    }
+  };
 
   useEffect(() => {
     if (isSuccess) {
       router.back();
     }
-  }, [isSuccess])
+  }, [isSuccess]);
 
-  return (
-    <ButtonSave onClick={onSave} />
-  );
-}
+  return <ButtonSave onClick={onSave} />;
+};
 
 interface PropsUpdateAction<T> {
-  id: number,
-  form: ReturnType<UseForm<T>>,
-  useSaveMutation: UseMutation<
-    MutationDefinition<
-      { id: number, body: T },
-      BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>, 
-      string,
-      unknown
-    >
-  >
+  id: number;
+  form: GenericForm<T>;
+  useUpdateMutation: GenericUpdateMutation<any, T>;
 }
 
-const UpdateAction = <T,>({
-  id,
-  form,
-  useSaveMutation
-}: PropsUpdateAction<T>) => {
+const UpdateAction = <T,>({ id, form, useUpdateMutation }: PropsUpdateAction<T>) => {
   const router = useRouter();
-  const [update, { isSuccess }] = useSaveMutation();
+  const [update, { isSuccess }] = useUpdateMutation();
 
   const onSave = () => {
     if (form.validate().hasErrors) {
-      update({ id, body: form.values })
-    } 
-  }
+      update({ id, body: form.values });
+    }
+  };
 
   useEffect(() => {
     if (isSuccess) {
       router.back();
     }
-  }, [isSuccess])
+  }, [isSuccess]);
 
-  return (
-    <ButtonSave onClick={onSave} />
-  );
-}
+  return <ButtonSave onClick={onSave} />;
+};
--- src/pages/dashboard-category/edit/[slug].tsx
+++ src/pages/dashboard-category/edit/[slug].tsx
@@ -1,11 +1,11 @@
 import { FormLayout } from '@/components/Common/Layout/FormLayout';
 import React, { useEffect } from 'react';
-import { DashboardCategoryDto } from '@/types/dashboard-category'
+import { DashboardCategoryDto } from '@/types/dashboard-category';
 import { useForm } from '@mantine/form';
 import { DashboardCategoryInputs } from '@/components/Forms/dashboard-category';
-import { 
+import {
   useUpdateDashboardCategoryMutation,
-  useFindDashboardCategoryQuery
+  useFindDashboardCategoryQuery,
 } from '@/store/dashboard-categories';
 import { useRouter } from 'next/router';
 import { skipToken } from '@reduxjs/toolkit/dist/query';
@@ -15,29 +15,29 @@ export default function OccupationCategoriesPage() {
   const { slug } = router.query;
   const id = parseInt(slug as string, 10);
 
-  const { data, isSuccess, isError, isLoading } =
-    useFindDashboardCategoryQuery(!!slug ? id : skipToken);
+  const { data, isSuccess, isError, isLoading } = useFindDashboardCategoryQuery(
+    !!slug ? id : skipToken
+  );
 
   const form = useForm<DashboardCategoryDto>({
     initialValues: {
-      name: ''
+      name: '',
     },
     validate: {
-      name: (values) => (
+      name: (values) =>
         values === undefined
           ? 'Campo nome da categoria é obrigatório'
           : values.length < 4
           ? 'O nome da categoria informado é muito curto'
-          : null
-      )
+          : null,
     },
-    validateInputOnChange: true
+    validateInputOnChange: true,
   });
 
   useEffect(() => {
     if (isSuccess) {
       form.setValues({
-        name: data.name
+        name: data.name,
       });
     }
   }, [isSuccess]);
@@ -45,12 +45,11 @@ export default function OccupationCategoriesPage() {
   return (
     <FormLayout
       title="Categorias de Ocupacoes"
-      useSaveMutation={useUpdateDashboardCategoryMutation}
+      useUpdateMutation={useUpdateDashboardCategoryMutation}
       type="update"
       FormInputs={DashboardCategoryInputs}
       form={form}
       id={id}
-    >
-    </FormLayout>
+    />
   );
 }
