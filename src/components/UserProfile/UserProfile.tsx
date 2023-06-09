e2ab44d1ccda24ffde480d57d6f6b1a35181dbee
helcio <helcio@carmo.in>
2023-06-09 13:14:01 -0300

    feat: Add logout on UserProfile component

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,27 +1,92 @@
-import React from 'react';
-import { Stack, Title, Text, Group, ActionIcon } from '@mantine/core';
+import React, { useState } from 'react';
+import { Stack, Text, Group, Menu, UnstyledButton, Avatar, createStyles, rem } from '@mantine/core';
 import { User } from '@/types/user';
-import { IconUser } from '@tabler/icons-react';
+import {
+  IconChevronDown,
+  IconLogout,
+  IconSettings,
+  IconSwitchHorizontal,
+  IconUser,
+} from '@tabler/icons-react';
+import { logout } from '@/store/auth';
+import { useRouter } from 'next/router';
 
 type Props = {
   user: User;
 };
 
+const useStyles = createStyles((theme) => ({
+  user: {
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.sm,
+    transition: 'background-color 100ms ease',
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      display: 'none',
+    },
+  },
+  userActive: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  },
+}));
+
 export const UserProfile: React.FC<Props> = ({ user }) => {
+  const router = useRouter();
+  const { classes, cx } = useStyles();
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+  const handleLogout = () => {
+    logout();
+    router.push('/auth/login');
+  };
+
   return (
     <Group spacing="lg">
-      <ActionIcon color="orange.3" variant="filled" size="xl" radius="md">
-        <IconUser color="white" size="32px" />
-      </ActionIcon>
-
-      <Stack spacing={0}>
-        <Title color="black" weight="bold" size="md">
-          {user.username}
-        </Title>
-        <Text color="dark.3" size="xs">
-          {user.username}
-        </Text>
-      </Stack>
+      <Menu
+        width={260}
+        shadow="md"
+        position="bottom-end"
+        transitionProps={{ transition: 'pop-top-right' }}
+        onClose={() => setUserMenuOpened(false)}
+        onOpen={() => setUserMenuOpened(true)}
+        withinPortal
+      >
+        <Menu.Target>
+          <UnstyledButton className={cx(classes.user, { [classes.userActive]: userMenuOpened })}>
+            <Group spacing="lg">
+              <Avatar alt={user.username} color="orange.3" variant="filled" size={44} radius="md">
+                <IconUser color="white" size={32} />
+              </Avatar>
+              <Stack spacing={0}>
+                <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
+                  {user.username}
+                </Text>
+                <Text color="dark.3" size="xs">
+                  {user.username}
+                </Text>
+              </Stack>
+              <IconChevronDown size={rem(12)} stroke={1.5} />
+            </Group>
+          </UnstyledButton>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Label>Configurações</Menu.Label>
+          <Menu.Item icon={<IconSettings size="0.9rem" stroke={1.5} />}>
+            Configurações da Conta
+          </Menu.Item>
+          <Menu.Item icon={<IconSwitchHorizontal size="0.9rem" stroke={1.5} />}>
+            Trocar Perfil de Acesso
+          </Menu.Item>
+          <Menu.Item onClick={handleLogout} icon={<IconLogout size="0.9rem" stroke={1.5} />}>
+            Sair
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
     </Group>
   );
 };
