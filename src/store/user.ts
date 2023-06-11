b338c880fee0cb7687bb88b5ff10438af3b0d0b5
helcio <helcio@carmo.in>
2023-06-11 19:53:09 -0300

    feat: Add user store

---
+++ src/store/user.ts
@@ -0,0 +1,20 @@
+import {
+  injectCreate,
+  injectRemove,
+  injectFindAll,
+  injectFindById,
+  injectUpdate,
+} from '@/store/common';
+import { CreateUserDto, User, UpdateUserDto } from '@/types/user';
+
+const endpoint = 'users';
+
+export const { useFindUserQuery } = injectFindById<User>('findUser', endpoint);
+
+export const { useFindAllUsersQuery } = injectFindAll<User>('findAllUsers', endpoint);
+
+export const { useCreateUserMutation } = injectCreate<User, CreateUserDto>('createUser', endpoint);
+
+export const { useUpdateUserMutation } = injectUpdate<User, UpdateUserDto>('updateUser', endpoint);
+
+export const { useRemoveUserMutation } = injectRemove('removeUser', endpoint);
