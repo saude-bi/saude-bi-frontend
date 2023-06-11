b4119ead8504cf4a1b954d8433195147f0ca6096
helcio <helcio@carmo.in>
2023-06-11 19:51:56 -0300

    feat: Add input validation medical-worker

--- src/components/Forms/medical-worker.tsx
+++ src/components/Forms/medical-worker.tsx
@@ -1,7 +1,8 @@
-import { UpdateDashboardCategoryDto } from '@/types/dashboard-category';
-import { Box, TextInput } from '@mantine/core';
+import { TextInput, InputBase, Stack, Radio, Group } from '@mantine/core';
 import { z } from 'zod';
-import { GenericForm } from '../Common/Layout/FormLayout';
+import { GenericForm } from '@/components/Common/Layout/FormLayout';
+import { IMaskInput } from 'react-imask';
+import { validateCNS, validateCPF } from '@/utils/validation';
 
 export const MedicalWorkerSchema = z.object({
   name: z
@@ -9,6 +10,37 @@ export const MedicalWorkerSchema = z.object({
       required_error: 'Campo nome da categoria é obrigatório',
     })
     .min(4, { message: 'O nome da categoria informado é muito curto' }),
+  user: z
+    .string({
+      required_error: 'Campo nome da usuário é obrigatório',
+    })
+    .min(4, { message: 'O nome de usuário informado é muito curto' }),
+  gender: z.enum(['F', 'M'], {
+    errorMap: (issue, _ctx) => {
+      switch (issue.code) {
+        case 'invalid_type':
+          return { message: 'Campo Sexo é obrigatório' };
+        case 'invalid_enum_value':
+          return { message: 'Campo Sexo é obrigatório' };
+        default:
+          return { message: 'Campo Sexo é obrigatório' };
+      }
+    },
+  }),
+  cns: z
+    .string({
+      required_error: 'Campo CNS é obrigatório',
+    })
+    .refine(validateCNS, {
+      message: 'CNS inválido',
+    }),
+  cpf: z
+    .string({
+      required_error: 'Campo CPF é obrigatório',
+    })
+    .refine(validateCPF, {
+      message: 'CPF inválido',
+    }),
 });
 
 type Props<T> = {
@@ -18,8 +50,18 @@ type Props<T> = {
 
 export const MedicalWorkerInputs = <T,>({ disabled = false, form }: Props<T>) => {
   return (
-    <Box>
+    <Stack>
       <TextInput
+        required
+        withAsterisk
+        label="Usuário"
+        placeholder="Selecione um usuário"
+        {...form.getInputProps('user')}
+        disabled={disabled}
+      />
+
+      <TextInput
+        required
         withAsterisk
         label="Nome do Profissional"
         placeholder="Nome do Profissional"
@@ -27,13 +69,34 @@ export const MedicalWorkerInputs = <T,>({ disabled = false, form }: Props<T>) =>
         disabled={disabled}
       />
 
-      <TextInput
+      <Stack spacing={2}>
+        <Radio.Group label="Sexo" {...form.getInputProps('gender')} withAsterisk>
+          <Group mt="xs">
+            <Radio value="F" label="Feminino" />
+            <Radio value="M" label="Masculino" />
+          </Group>
+        </Radio.Group>
+      </Stack>
+
+      <InputBase
+        component={IMaskInput}
+        mask="000.000.000-00"
         withAsterisk
-        label="Nome do Profissional"
-        placeholder="Nome do Profissional"
-        {...form.getInputProps('name')}
+        label="CPF"
+        placeholder="CPF do Profissional"
+        {...form.getInputProps('cpf')}
         disabled={disabled}
       />
-    </Box>
+
+      <InputBase
+        component={IMaskInput}
+        mask="000000000000000"
+        withAsterisk
+        label="Cartão SUS"
+        placeholder="CNS do Profissional"
+        {...form.getInputProps('cns')}
+        disabled={disabled}
+      />
+    </Stack>
   );
 };
---
+++ src/utils/validation.ts
@@ -0,0 +1,127 @@
+export const validateCNS = (cns: string): boolean => {
+  if (cns.trim().length != 15) {
+    return false;
+  }
+
+  if (cns[0] != '1' && cns[0] != '2') {
+    return validateCns12(cns);
+  }
+  return validateCns789(cns);
+};
+
+export const validateCns12 = (cns: string): boolean => {
+  let soma: number;
+  let resto, dv: number;
+  let pis: string = '';
+  let resultado: string = '';
+  pis = cns.substring(0, 11);
+
+  soma =
+    parseInt(pis.substring(0, 1)) * 15 +
+    parseInt(pis.substring(1, 2)) * 14 +
+    parseInt(pis.substring(2, 3)) * 13 +
+    parseInt(pis.substring(3, 4)) * 12 +
+    parseInt(pis.substring(4, 5)) * 11 +
+    parseInt(pis.substring(5, 6)) * 10 +
+    parseInt(pis.substring(6, 7)) * 9 +
+    parseInt(pis.substring(7, 8)) * 8 +
+    parseInt(pis.substring(8, 9)) * 7 +
+    parseInt(pis.substring(9, 10)) * 6 +
+    parseInt(pis.substring(10, 11)) * 5;
+
+  resto = soma % 11;
+  dv = 11 - resto;
+
+  if (dv == 11) {
+    dv = 0;
+  }
+
+  if (dv == 10) {
+    soma =
+      parseInt(pis.substring(0, 1)) * 15 +
+      parseInt(pis.substring(1, 2)) * 14 +
+      parseInt(pis.substring(2, 3)) * 13 +
+      parseInt(pis.substring(3, 4)) * 12 +
+      parseInt(pis.substring(4, 5)) * 11 +
+      parseInt(pis.substring(5, 6)) * 10 +
+      parseInt(pis.substring(6, 7)) * 9 +
+      parseInt(pis.substring(7, 8)) * 8 +
+      parseInt(pis.substring(8, 9)) * 7 +
+      parseInt(pis.substring(9, 10)) * 6 +
+      parseInt(pis.substring(10, 11)) * 5 +
+      2;
+
+    resto = soma % 11;
+    dv = 11 - resto;
+    resultado = pis + '001' + dv.toString();
+  } else {
+    resultado = pis + '000' + dv.toString();
+  }
+
+  if (!(cns == resultado)) {
+    return false;
+  }
+  return true;
+};
+
+export const validateCns789 = (cns: string): boolean => {
+  let dv: number;
+  let resto, soma: number;
+
+  soma =
+    parseInt(cns.substring(0, 1)) * 15 +
+    parseInt(cns.substring(1, 2)) * 14 +
+    parseInt(cns.substring(2, 3)) * 13 +
+    parseInt(cns.substring(3, 4)) * 12 +
+    parseInt(cns.substring(4, 5)) * 11 +
+    parseInt(cns.substring(5, 6)) * 10 +
+    parseInt(cns.substring(6, 7)) * 9 +
+    parseInt(cns.substring(7, 8)) * 8 +
+    parseInt(cns.substring(8, 9)) * 7 +
+    parseInt(cns.substring(9, 10)) * 6 +
+    parseInt(cns.substring(10, 11)) * 5 +
+    parseInt(cns.substring(11, 12)) * 4 +
+    parseInt(cns.substring(12, 13)) * 3 +
+    parseInt(cns.substring(13, 14)) * 2 +
+    parseInt(cns.substring(14, 15)) * 1;
+
+  resto = soma % 11;
+
+  if (resto != 0) {
+    return false;
+  }
+  return true;
+};
+
+export const validateCPF = (cpf: string): boolean => {
+  cpf = cpf.replace(/[^\d]+/g, '');
+  if (cpf == '') return false;
+  // Elimina CPFs invalidos conhecidos
+  if (
+    cpf.length != 11 ||
+    cpf == '00000000000' ||
+    cpf == '11111111111' ||
+    cpf == '22222222222' ||
+    cpf == '33333333333' ||
+    cpf == '44444444444' ||
+    cpf == '55555555555' ||
+    cpf == '66666666666' ||
+    cpf == '77777777777' ||
+    cpf == '88888888888' ||
+    cpf == '99999999999'
+  )
+    return false;
+  // Valida 1o digito
+  let add: number = 0;
+  for (let i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
+  let rev: number = 11 - (add % 11);
+  if (rev == 10 || rev == 11) rev = 0;
+  if (rev != parseInt(cpf.charAt(9))) return false;
+  // Valida 2o digito
+  add = 0;
+  for (let i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+  rev = 11 - (add % 11);
+  if (rev == 10 || rev == 11) rev = 0;
+  if (rev != parseInt(cpf.charAt(10))) return false;
+  return true;
+};
