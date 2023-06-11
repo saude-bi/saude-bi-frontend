b7556a955cc050470b922fa434402712ad4bab48
helcio <helcio@carmo.in>
2023-06-11 19:52:55 -0300

    feat: Add create and update UserDto

--- src/types/user.ts
+++ src/types/user.ts
@@ -5,6 +5,13 @@ export interface User extends Entity {
   isAdmin: boolean;
 }
 
+export type CreateUserDto = {
+  username: string;
+  password: string;
+};
+
+export type UpdateUserDto = Partial<CreateUserDto>;
+
 export type LoginFormDto = {
   username: string;
   password: string;
