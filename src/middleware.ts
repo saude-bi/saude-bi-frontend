ddcb468e03e6f77c3f662d2324e0c28c6f8d8e87
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-06-12 17:36:45 -0300

    feat: (WIP) Middleware for admin endpoints

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const isUserRoute = (pathname: string) => {
+    return pathname.startsWith('/occupation');
+}
+
+export async function middleware(req: NextRequest) {
+  const role = req.headers.get("authorization");
+  const { pathname } = req.nextUrl;
+  console.log('teste middleware');
+  console.log(req.headers);
+
+  if (isUserRoute(pathname)) {
+    return NextResponse.next();
+    //return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/occupation/:path*', '/medical-worker/:path*']
+};
