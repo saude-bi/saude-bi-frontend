01102372c28d6023ca0d475d07c14757172a26da
helcio <helcio@carmo.in>
2023-06-18 15:15:39 -0300

    feat: Add loading page component

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { LoadingOverlay } from '@mantine/core';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LoadingOverlay visible />;
+}
