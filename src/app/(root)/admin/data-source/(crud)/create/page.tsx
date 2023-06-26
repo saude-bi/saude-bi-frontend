3d7eaa63729ec66a8a1c2c46c8b7070c6ce5e84b
edua2127 <magalhaeseduardo2001@gmail.com>
2023-06-26 17:13:54 -0300

    desenvolvimento do create

--- src/app/(root)/admin/data-source/(crud)/create/page.tsx
+++ src/app/(root)/admin/data-source/(crud)/create/page.tsx
@@ -5,7 +5,7 @@ import { useForm, zodResolver } from '@mantine/form';
 import { FormLayout } from '@/components/Common/Layout/FormLayout';
 import { CreateDataSourceDto } from '@/types/data-source';
 import { DataSourceInputs, DataSourceSchema } from '@/components/Forms/data-source';
-import { useCreateEstablishmentMutation } from '@/store/establishments';
+
 import { useCreateDataSourceMutation } from '@/store/data-source';
 
 export default function DataSourcePage() {
