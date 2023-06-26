6a40487df5f633b6bbcbf4e2b69a1471091d363a
helcio <helcio@carmo.in>
2023-06-26 19:48:38 -0300

    feat: Open dashboard with filter

--- src/store/dashboards.ts
+++ src/store/dashboards.ts
@@ -4,19 +4,28 @@ import {
   injectFindAll,
   injectUpdate,
   injectCreate,
+  injectFindByIdChild,
 } from '@/store/common';
 
-import { Dashboard, CreateDashboardDto, UpdateDashboardDto } from "@/types/dashboards"
+import {
+  Dashboard,
+  CreateDashboardDto,
+  UpdateDashboardDto,
+  FindDashboardUrlDto,
+} from '@/types/dashboards';
 
 const endpoint = 'dashboards';
 
+const endpointUrl = 'dashboards/[slug]/url';
 
 export const { useFindDashboardQuery } = injectFindById<Dashboard>('findDashboard', endpoint);
 
-export const { useFindAllDashboardQuery } = injectFindAll<Dashboard>(
-  'findAllDashboard',
-  endpoint
-);
+export const { useFindDashboardUrlQuery } = injectFindByIdChild<
+  FindDashboardUrlDto,
+  { workRelation: number }
+>('findDashboardUrl', endpointUrl);
+
+export const { useFindAllDashboardQuery } = injectFindAll<Dashboard>('findAllDashboard', endpoint);
 
 export const { useCreateDashboardMutation } = injectCreate<Dashboard, CreateDashboardDto>(
   'createDashboard',
--- src/types/dashboards.ts
+++ src/types/dashboards.ts
@@ -1,25 +1,28 @@
 import { Entity } from '@/types/common';
-import { Establishment } from "./establishment";
+import { Establishment } from './establishment';
 import { DashboardCategory } from './dashboard-category';
 import { DataSource } from './data-source';
 
 export interface Dashboard extends Entity {
-    establishmentsWithAccess: Establishment[],
-    dataSource: DataSource;
-    category: DashboardCategory;
-    metabaseId: number;
-    name: string;
-    establishmentPropertyName: string
+  establishmentsWithAccess: Establishment[];
+  dataSource: DataSource;
+  category: DashboardCategory;
+  metabaseId: number;
+  name: string;
+  establishmentPropertyName: string;
+}
+
+export interface FindDashboardUrlDto extends Entity {
+  url: string;
 }
 
 export type CreateDashboardDto = {
-    establishmentsWithAccess: number[],
-    dataSource: number;
-    category: number;
-    metabaseId: number;
-    name: string;
-    establishmentPropertyName: string;
+  establishmentsWithAccess: number[];
+  dataSource: number;
+  category: number;
+  metabaseId: number;
+  name: string;
+  establishmentPropertyName: string;
 };
-  
-  export type UpdateDashboardDto = Partial<CreateDashboardDto>;
-  
+
+export type UpdateDashboardDto = Partial<CreateDashboardDto>;
