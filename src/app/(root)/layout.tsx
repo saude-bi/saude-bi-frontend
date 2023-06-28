bb418deded6d40dcb365d210a669119361ec86f6
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-06-28 16:31:36 -0300

    refactor: Move CommonLayout to layout from template

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,12 @@
-import { AuthProvider } from "@/context/auth";
+import { AuthProvider } from '@/context/auth';
+import { CommonLayout } from "@/components/Common/Layout/CommonLayout";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      {children}
+      <CommonLayout>
+        {children}
+      </CommonLayout>
     </AuthProvider>
   );
 }
--- src/app/(root)/template.tsx
+++ src/app/(root)/template.tsx
@@ -1,7 +1,4 @@
-'use client';
-
-import { CommonLayout } from '@/components/Common/Layout/CommonLayout';
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <CommonLayout title="Dashboards">{children}</CommonLayout>;
+  return children;
 }
