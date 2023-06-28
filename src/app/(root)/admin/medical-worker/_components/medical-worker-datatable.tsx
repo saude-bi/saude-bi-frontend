35c2ea671a858467c2136e90559716954ea12bf4
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-06-28 16:33:26 -0300

    refactor: split medical-worker datatable from page

---
+++ src/app/(root)/admin/medical-worker/_components/medical-worker-datatable.tsx
@@ -0,0 +1,33 @@
+
+'use client'
+
+import { useMemo } from 'react';
+import { MRT_ColumnDef } from 'mantine-react-table';
+import { DataTable } from '@/components/Common/DataTable/DataTable';
+import {
+  useFindAllMedicalWorkersQuery,
+  useRemoveMedicalWorkerMutation,
+} from '@/store/medical-worker';
+import { MedicalWorker } from '@/types/medical-worker';
+
+export default function MedicalWorkerDatatable() {
+  const columns = useMemo<MRT_ColumnDef<MedicalWorker>[]>(
+    () => [
+      { accessorKey: 'name', header: 'Nome' },
+      { accessorKey: 'created', header: 'Data de Criacao' },
+      { accessorKey: 'updated', header: 'Data de Atualizacao' },
+    ],
+    []
+  );
+
+  return (
+    <DataTable
+      useFindAllQuery={useFindAllMedicalWorkersQuery}
+      useRemoveMutation={useRemoveMedicalWorkerMutation}
+      columns={columns}
+      canCreate
+      canUpdate
+      canPreview
+    />
+  );
+}
--- src/app/(root)/admin/medical-worker/page.tsx
+++ src/app/(root)/admin/medical-worker/page.tsx
@@ -1,32 +1,7 @@
-'use client';
-
-import { useMemo } from 'react';
-import { MRT_ColumnDef } from 'mantine-react-table';
-import { DataTable } from '@/components/Common/DataTable/DataTable';
-import {
-  useFindAllMedicalWorkersQuery,
-  useRemoveMedicalWorkerMutation,
-} from '@/store/medical-worker';
-import { MedicalWorker } from '@/types/medical-worker';
+import MedicalWorkerDatatable from './_components/medical-worker-datatable';
 
 export default function MedicalWorkerPage() {
-  const columns = useMemo<MRT_ColumnDef<MedicalWorker>[]>(
-    () => [
-      { accessorKey: 'name', header: 'Nome' },
-      { accessorKey: 'created', header: 'Data de Criacao' },
-      { accessorKey: 'updated', header: 'Data de Atualizacao' },
-    ],
-    []
-  );
-
   return (
-    <DataTable
-      useFindAllQuery={useFindAllMedicalWorkersQuery}
-      useRemoveMutation={useRemoveMedicalWorkerMutation}
-      columns={columns}
-      canCreate
-      canUpdate
-      canPreview
-    />
+    <MedicalWorkerDatatable />
   );
 }
