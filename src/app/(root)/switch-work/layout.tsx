129796e2451be4508d656075a9a2a43a3e0beba2
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2023-06-29 17:05:04 -0300

    feat: Server side directorship request on home

--- src/app/(root)/switch-work/layout.tsx
+++ src/app/(root)/switch-work/layout.tsx
@@ -1,15 +1,52 @@
-import { NavBar } from '@/components/Common/NavBar/NavBar';
 import React from 'react';
+import { NavBar } from '@/components/Common/NavBar/NavBar';
+import { SwitchWorkLayout } from './_components/switch-work-layout';
 
 export const metadata = {
   title: 'Alterar Vínculo'
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { PaginatedResponse } from '@/types/common';
+import { Directorship } from '@/types/directorship';
+import { cookies } from 'next/dist/client/components/headers';
+
+async function getData() {
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set('Content-Type', 'application/json');
+  requestHeaders.set('credentials', 'same-origin');
+  requestHeaders.set('mode', 'cors');
+
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')
+
+  if (token) {
+    requestHeaders.set('Authorization', `Bearer ${token}`);
+  }
+
+  const res = await fetch('http://localhost:8000/directorships', {
+    method: 'GET',
+    headers: requestHeaders,
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return await res.json() as PaginatedResponse<Directorship>
+}
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const directorship = await getData();
   return (
     <>
       <NavBar title="Alterar Vínculo"></NavBar>
-      {children}
+      <SwitchWorkLayout directorship={directorship.data}>
+        {children}
+      </SwitchWorkLayout>
     </>
   );
 }
--- src/app/(root)/switch-work/page.tsx
+++ src/app/(root)/switch-work/page.tsx
@@ -1,65 +1,18 @@
-'use client';
+'use client'
 
-import { useEffect, useState } from 'react';
-import { ContentCard } from '@/components/Common/ContentCard/ContentCard';
-import { Anchor, Group, Stack, Text, Title } from '@mantine/core';
-import { FilterSelector } from '@/components/FilterSelector/FilterSelector';
-import { useGetCurrentUserQuery } from '@/store/auth';
-import { setCookie } from 'cookies-next';
-import { useRouter } from 'next/navigation';
+import { useGetCurrentUserQuery } from "@/store/auth";
+import { Group, Title } from "@mantine/core";
+import { useState } from "react";
+import { AccessCard } from "./_components/access-card";
 
-type Props = {
-  establishment: string;
-  occupation: string;
-  workRelation: number;
-};
 
-const AccessCard: React.FC<Props> = ({ establishment, occupation, workRelation }) => {
-  const router = useRouter();
-
-  const onClick = () => {
-    setCookie('workRelation', workRelation.toString());
-    router.push('/');
-  };
-
-  return (
-    <Anchor onClick={onClick} style={{ textDecoration: 'none' }}>
-      <ContentCard>
-        <Stack spacing={'sm'}>
-          <Text fz="sm" fw={500}>
-            {establishment}
-          </Text>
-          <Text component="p" sx={{ margin: 0 }}>
-            {occupation}
-          </Text>
-        </Stack>
-      </ContentCard>
-    </Anchor>
-  );
-};
-
-export default function Home() {
+export default async function Home() {
   const { currentData: currentUser } = useGetCurrentUserQuery();
   const [workRelations] = useState(currentUser?.medicalWorker?.workRelations);
-
-  useEffect(() => {
-    console.log(currentUser);
-  }, [currentUser]);
-
   return (
-    <Stack>
-      <ContentCard>
-        <Stack align="center" justify="center">
-          <Text>Filtrar por Diretoria:</Text>
-          <FilterSelector
-            options={['DIAT', 'DIAC', 'DIES', 'DIFC', 'DIRS', 'DIVS', 'DIEC']}
-            setSelectedOption={() => {}}
-          />
-        </Stack>
-      </ContentCard>
-      <Stack>
-        <Title order={4}>Selecione o acesso que deseja utilizar:</Title>
-        <Group>
+    <>
+      <Title order={4}>Selecione o acesso que deseja utilizar:</Title>
+      <Group>
           {workRelations?.map((workRelation) => (
             <AccessCard
               establishment={workRelation.establishment?.name}
@@ -67,8 +20,9 @@ export default function Home() {
               workRelation={workRelation.id}
             />
           ))}
+          
         </Group>
-      </Stack>
-    </Stack>
+
+    </>
   );
 }
